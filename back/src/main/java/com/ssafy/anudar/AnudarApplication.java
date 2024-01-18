f5e6b36e21aabe0a5439505b508f5d3dcd66aa59
sarang682 <jms20914@gmail.com>
2024-01-18 15:02:43 +0900

    React 프로젝트 생성

--- back/src/main/java/com/ssafy/anudar/AnudarApplication.java
+++ back/src/main/java/com/ssafy/anudar/AnudarApplication.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:baf71c8369594e5eac866280f74590dcc498f7a048b56b3052ceade82bca3659
-size 321
+package com.ssafy.anudar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnudarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnudarApplication.class, args);
+    }
+
+}
--- back/src/test/java/com/ssafy/anudar/AnudarApplicationTests.java
+++ back/src/test/java/com/ssafy/anudar/AnudarApplicationTests.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a8b8283e0005525b51d1f7ae645089b2a917e11dde94d7f768202e641d8154a7
-size 217
+package com.ssafy.anudar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnudarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

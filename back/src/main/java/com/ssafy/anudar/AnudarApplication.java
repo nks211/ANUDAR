33721053f6d200f8e50d26b254002b89843bc776
sarang682 <jms20914@gmail.com>
2024-01-18 14:50:58 +0900

    add .gitignore

--- back/src/main/java/com/ssafy/anudar/AnudarApplication.java
+++ back/src/main/java/com/ssafy/anudar/AnudarApplication.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85e269a1f7794fd56654611693f4a8a621f68860e434a7e695dc6416ea101533
-size 334
+oid sha256:baf71c8369594e5eac866280f74590dcc498f7a048b56b3052ceade82bca3659
+size 321

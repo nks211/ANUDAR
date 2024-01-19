3302b8d410a1beeb826f22c241e80ef0a71fb7ce
sarang682 <jms20914@gmail.com>
2024-01-19 14:55:45 +0900

    feat: back: 로그인

---
+++ back/src/main/java/com/ssafy/anudar/config/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.ssafy.anudar.config;
+
+import com.ssafy.anudar.config.filter.JwtTokenFilter;
+import com.ssafy.anudar.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+
+    private final UserService userService;
+
+    @Value("${jwt.secret}")
+    private String key;
+
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers("/user/login")
+                .requestMatchers("/user/join")
+                ;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/user/join").permitAll()
+                        .requestMatchers("/user/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        http
+                .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
---
+++ back/src/main/java/com/ssafy/anudar/dto/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.ssafy.anudar.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    String username;
+    String password;
+
+}

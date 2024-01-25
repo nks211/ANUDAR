dc4b7d25c7893bc4a955d56399fac8b2d506dd08
suyeon-hyeon <shonee99@naver.com>
2024-01-25 17:38:36 +0900

    fix: entity

--- back/src/main/java/com/ssafy/anudar/model/User.java
+++ back/src/main/java/com/ssafy/anudar/model/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f27cb810a34b096c867c892344f4f9ed927b19f997ae0b0a78bc16754f67f98a
-size 1771
+oid sha256:2d4caa6260c5bcb26981fa112bc58a940b2003b7e6926c476907fea7407be7da
+size 1932

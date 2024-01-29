8685a216274b7b698d5a0b750607d5a12bbb7a70
김민우 <kmw7532@naver.com>
2024-01-29 17:07:16 +0900

    update!

--- front/src/components/search/Search.js
+++ front/src/components/search/Search.js
@@ -3,32 +3,6 @@ import './Search.css'
 
 export default function Search(props) {
   const [searchValue, setSearchValue] = useState("");
-<<<<<<< .merge_file_xkV6IX
-  return (
-    <div className="search">
-      {/* <div className="searchArea"> */}
-
-
-      <input id="searchValue" type="text" onChange={event=>{
-        setSearchValue(event.target.value)
-        // console.log(event.target.value)
-        // console.log(searchValue)
-        }}/>
-      {/* <input type="submit" value="검색"/> */}
-      <button onClick={()=>{
-        // console.log(searchValue)
-        props.updateWorks(searchValue)
-        }}>검색</button>
-
-      {/* </div> */}
-    </div>
-
-    // <div className="search">
-    //   <input type="text" />
-    //   <label>Name</label>
-    //   <span></span>
-    // </div>
-=======
 
   return (
     <div className="search">
@@ -44,6 +18,5 @@ export default function Search(props) {
       onClick={()=>{props.updateValues(searchValue)}}
       >검색</button>
     </div>
->>>>>>> .merge_file_44bTUI
   )
 }

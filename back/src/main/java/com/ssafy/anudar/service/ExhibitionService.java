4e4e784b823f7eff238ebde470ce23997db5af5c
suhyeon cho <jeniffer0812@naver.com>
2024-01-30 16:21:02 +0900

    merge 완료

--- back/src/main/java/com/ssafy/anudar/service/ExhibitionService.java
+++ back/src/main/java/com/ssafy/anudar/service/ExhibitionService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e56cd6ea16e084cfec329c168073ba5d5c8aa7f24a8d3609239a96e0a6f25583
-size 4666
+oid sha256:d573e91e05c643af9ac8ae19942b0cc41b375eb739b2866efd4b131fce7075bf
+size 4676

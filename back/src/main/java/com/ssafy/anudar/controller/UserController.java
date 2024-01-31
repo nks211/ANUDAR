bfdf9300e54f8262fcee73d8831bfc448270d589
안유나 <yunanash1234@gmail.com>
2024-01-31 17:58:06 +0900

    fix: back: 회원정보수정완

--- back/src/main/java/com/ssafy/anudar/controller/UserController.java
+++ back/src/main/java/com/ssafy/anudar/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55fc8b2eaa51c0f4055c7d81a18caeaf309d4323091f3da72664ea4a66220ac4
-size 4212
+oid sha256:729ba9f3a3c8d17224057846268e7a7943957693ed23631ea30c7e509bdfc607
+size 4213

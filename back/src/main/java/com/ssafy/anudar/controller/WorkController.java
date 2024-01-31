7cf683c74af99802d0e82327d62bc6c644dda36a
안유나 <yunanash1234@gmail.com>
2024-01-31 16:32:22 +0900

    fix: back: 찜하기 취소

--- back/src/main/java/com/ssafy/anudar/controller/WorkController.java
+++ back/src/main/java/com/ssafy/anudar/controller/WorkController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f5d45df7e0ed021ae48e5b070353bc57e00f39e17d0d3b896db54754e379d6b
-size 2651
+oid sha256:34373eb9597fd49aec37d7275f9bd0eaaf49ed2014023ae2bfbaeda6c775e4a8
+size 2653

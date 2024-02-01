35951cb43dfe5ce0bd3bf01ea8dcefdd06121de9
김민우 <kmw7532@naver.com>
2024-02-01 17:38:58 +0900

    update: 마이페이지 레이아웃수정, modal 라이브러리 기반 모달창 구현

--- front/src/components/checkpassword.jsx
+++ front/src/components/checkpassword.jsx
@@ -1,18 +1,3 @@
-import { React, useContext } from "react";
-import "./checkpassword.css";
-import { MypageContext } from "../mypage/mypage";
-
-export default function CheckPassword() {
-
-    const { passwordcheck, setPasswordCheck } = useContext(MypageContext);
-
-    return (
-        <div className="checkpasswordarea">
-            <div className="checkpasswordtitle">비밀번호 입력</div>
-            <div className="checkpassworddetail">본인 확인을 위해 비밀번호를 입력해주세요.</div>
-            <input className="checkpasswordinput" type="password" placeholder="비밀번호" />
-            <div onClick={() => { setPasswordCheck(false); }} className="checkpasswordbutton">확인</div>
-        </div>
-    );
-
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:30046a840db1e3d1978a51338a920cd4b7c5ccd15c4f4c270dd7ad5c9cf01962
+size 726

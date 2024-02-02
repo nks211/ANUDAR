79e7448546e4721a017092e2dc5afb7d6bd28fd1
sarang682 <jms20914@gmail.com>
2024-02-02 21:44:41 +0900

    fix: back: redis 저장 데이터 수정

--- back/src/main/java/com/ssafy/anudar/model/UserPrincipalDetails.java
+++ back/src/main/java/com/ssafy/anudar/model/UserPrincipalDetails.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:830559e0904b68f2c82372a49cb99d814ac858ebb87abd5004e0f828d37bdc31
-size 1844
+oid sha256:4591cb550f63fefec7b1d0b12ca21452e75cd46c5961065a94ba89356a689d60
+size 2060

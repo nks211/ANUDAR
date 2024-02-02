82a9307caa3a2525da8dfda14d979310e1897a00
sarang682 <jms20914@gmail.com>
2024-02-02 10:53:15 +0900

    feat: back: recording&save video

--- back/src/main/java/com/ssafy/anudar/S3/FileFolder.java
+++ back/src/main/java/com/ssafy/anudar/S3/FileFolder.java
@@ -1,5 +1,3 @@
-package com.ssafy.anudar.S3;
-
-public enum FileFolder {
-    USER_IMG, EXHIBIT_IMG
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:552ee6bef524b12f470b1309ec0ecd737db5c2a0d3f5798b863962ef27914178
+size 90

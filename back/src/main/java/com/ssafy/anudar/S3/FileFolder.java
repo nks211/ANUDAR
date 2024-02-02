bdd7373446153bacf934b6c0d9801805bac3ee1f
sarang682 <jms20914@gmail.com>
2024-02-02 13:25:14 +0900

    fix: back: recording 수정

--- back/src/main/java/com/ssafy/anudar/S3/FileFolder.java
+++ back/src/main/java/com/ssafy/anudar/S3/FileFolder.java
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:552ee6bef524b12f470b1309ec0ecd737db5c2a0d3f5798b863962ef27914178
-size 90
+package com.ssafy.anudar.S3;
+
+public enum FileFolder {
+    USER_IMG, EXHIBIT_IMG, VIDEO
+}

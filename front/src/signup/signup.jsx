550f9283a00674acfb9f1b820b08658415e88f26
김민우 <kmw7532@naver.com>
2024-02-02 17:00:39 +0900

    feat: zustand 상태 관리 함수 제작

--- front/src/signup/signup.jsx
+++ front/src/signup/signup.jsx
@@ -14,6 +14,7 @@ function Signup() {
 
     const upload = (e) => {
         const file = e.target.files[0];
+        if (file === null) return;
         const reader = new FileReader();
         reader.readAsDataURL(file);
 

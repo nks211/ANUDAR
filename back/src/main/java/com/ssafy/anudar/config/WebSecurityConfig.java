99892db42f9366df8470ac28266c060593e0648a
sarang682 <jms20914@gmail.com>
2024-02-03 18:11:11 +0900

    feat: back: 채팅 기능 구현

--- back/src/main/java/com/ssafy/anudar/config/WebSecurityConfig.java
+++ back/src/main/java/com/ssafy/anudar/config/WebSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8903784981913a5b54a7938dd7decdefd0283092ba55d9091f1a9800af561281
-size 3835
+oid sha256:2d6eebcf8e41713ef29bd7d6179259204f2a0dfd02cf9afb2841d82bad8037d3
+size 3878

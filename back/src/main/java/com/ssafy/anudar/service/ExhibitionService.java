b5bac16e29ced4c5b350cd05bbbee2ad2a453089
sarang682 <jms20914@gmail.com>
2024-02-04 14:10:27 +0900

    refacrot: back: 전시회 상세 조회

--- back/src/main/java/com/ssafy/anudar/service/ExhibitionService.java
+++ back/src/main/java/com/ssafy/anudar/service/ExhibitionService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3f2ed4c9db6b8b02b56ec3c33f1634ab0fea354a94039cd34c0434dfb633112
-size 5004
+oid sha256:652adb848ee15f069cebef9430e5f7acfcf110657420eb284943db7f00bbb62a
+size 5162

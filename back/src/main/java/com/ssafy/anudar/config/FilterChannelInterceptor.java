059fb53527f68f837004c5f60b69a5618cd2aab0
안유나 <yunanash1234@gmail.com>
2024-02-05 10:48:13 +0900

    fix: back: 시큐리티

--- back/src/main/java/com/ssafy/anudar/config/FilterChannelInterceptor.java
+++ back/src/main/java/com/ssafy/anudar/config/FilterChannelInterceptor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:010a2e080edccf7436a3c083ea7e6e47c7d0005f3dae376e5399fb866ae5a437
-size 1524
+oid sha256:087aa7900f03909264a79ddb1d34afa17099e8fae6154094fb98b93c4853e54a
+size 2127

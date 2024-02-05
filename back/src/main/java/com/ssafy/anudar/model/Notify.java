8e373c4d0d4e734ae75ce8fd61ceb5eee7b9428d
suhyeon cho <jeniffer0812@naver.com>
2024-02-05 14:09:25 +0900

    notify: jsonignore 추가

--- back/src/main/java/com/ssafy/anudar/model/Notify.java
+++ back/src/main/java/com/ssafy/anudar/model/Notify.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @Entity

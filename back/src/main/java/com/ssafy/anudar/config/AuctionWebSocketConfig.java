5b484b503abf620eca56805ac6e35bb260916a40
안유나 <yunanash1234@gmail.com>
2024-02-05 16:45:57 +0900

    remove: back: 시큐리티 보류

--- back/src/main/java/com/ssafy/anudar/config/AuctionWebSocketConfig.java
+++ back/src/main/java/com/ssafy/anudar/config/AuctionWebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:494588b3e676d985d0c43951698fcf9a7532b5ce35aa123737fcf28a7712ab53
-size 1659
+oid sha256:46a34d767efa33206d5e2492d9b81a4b6cfc0b678a096bcb27f6f47fea2e73c5
+size 1669
--- back/src/main/java/com/ssafy/anudar/config/FilterChannelInterceptor.java
+++ back/src/main/java/com/ssafy/anudar/config/FilterChannelInterceptor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:087aa7900f03909264a79ddb1d34afa17099e8fae6154094fb98b93c4853e54a
-size 2127
+oid sha256:e5c0c9a967a589755c2946af8685ffdd8c3837134c61b1596e14c7eecc1fd363
+size 2217

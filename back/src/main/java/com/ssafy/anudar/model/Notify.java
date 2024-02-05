2e7569102053f581c8b2bfd1bca6ad397cb6e083
suhyeon cho <jeniffer0812@naver.com>
2024-02-05 13:59:23 +0900

    fix: notify (jsonIgnore 안붙임)

--- back/src/main/java/com/ssafy/anudar/model/Notify.java
+++ back/src/main/java/com/ssafy/anudar/model/Notify.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c530c047d5cffd9e3a6e495cd89abeabe5e62a968e82a624ba76e9f39972bf5
-size 1267
+oid sha256:97d06ffa899676cb2cc96df7986f0f5caf08289a0db33ce2560a7893c95e5eaa
+size 1335

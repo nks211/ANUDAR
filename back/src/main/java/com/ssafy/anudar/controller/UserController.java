83531d8bb11f104f2180546f1cf9d2fc55c72217
sarang682 <jms20914@gmail.com>
2024-02-06 17:02:17 +0900

    fix: back: 에러 수정

--- back/src/main/java/com/ssafy/anudar/controller/UserController.java
+++ back/src/main/java/com/ssafy/anudar/controller/UserController.java
@@ -21,7 +21,6 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-    private final NotifyService notifyService;
     private final UserService userService;
     private final S3Service s3Service;
 

acf0f28f27ea567fb9f32027cd3e8fb1311355c5
YUNA-AHN <yunanash1234@gmail.com>
2024-02-07 20:40:58 +0900

    fix: websocket: retry

--- back/src/main/java/com/ssafy/anudar/config/WebSocketConfig.java
+++ back/src/main/java/com/ssafy/anudar/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:017c48848f8e2cf96dd54a32b3f52cbbe5693620be9bc91c8c1c9382a547c667
-size 1887
+oid sha256:c2707e4458d2d165fd6ea05d1194cdff51934edd7d2a0514b90aaec1ed4a5555
+size 1888

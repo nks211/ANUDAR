2bc1b68ec049e505f94cb9eb632b26fbaf370161
안유나 <yunanash1234@gmail.com>
2024-02-07 15:55:27 +0900

    fix: back : 시큐리티 로컬스토리지 이용

--- front/src/auctionlive/AuctionLivePage.jsx
+++ front/src/auctionlive/AuctionLivePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55b22e428215e8f15511c15e18a2039700025b29a8e18c281ce49f5f2b6d5a48
-size 3266
+oid sha256:fae74d503f0629e53e4fe8ac236508b816fce1fdde7bbddbd054a7a6d3624743
+size 3052

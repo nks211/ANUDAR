2af692249b1b319d6b66a0c435c36856201dd36d
안유나 <yunanash1234@gmail.com>
2024-02-07 16:08:34 +0900

    fix: front: 배포서버로 변셜

--- front/src/auctionlive/AuctionLivePage.jsx
+++ front/src/auctionlive/AuctionLivePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fae74d503f0629e53e4fe8ac236508b816fce1fdde7bbddbd054a7a6d3624743
-size 3052
+oid sha256:b9e42f5df1b3a6fd753639b337f618abdca32fb39e542f31c50c9fc610589700
+size 3056

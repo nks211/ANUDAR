3068b6c975fd4ab87ba7f57c00398b896c1372e2
안유나 <yunanash1234@gmail.com>
2024-02-07 17:17:08 +0900

    fix: 웹소켓..

--- back/src/main/java/com/ssafy/anudar/config/WebSocketConfig.java
+++ back/src/main/java/com/ssafy/anudar/config/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe53f7d8ffe1ee522622a001f4fdcdac6ee23106fb38d8ab5aaafe66d17821a1
-size 1844
+oid sha256:017c48848f8e2cf96dd54a32b3f52cbbe5693620be9bc91c8c1c9382a547c667
+size 1887
--- front/src/docent/DocentPage.jsx
+++ front/src/docent/DocentPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d935464b0dbde3e584e4bb4765c5fe140f481c9c847c5015ad2704aa834c400
-size 3868
+oid sha256:146364273a89964ab75ea3e4bfe5aca1e830c3ebfea5e36a96f0fd2bae077a92
+size 3719

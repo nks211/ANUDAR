3b69473527a950060c2626e1235202763f33da47
김민우 <kmw7532@naver.com>
2024-02-08 14:57:31 +0900

    back update

--- back/src/main/java/com/ssafy/anudar/repository/AuctionWorkRepository.java
+++ back/src/main/java/com/ssafy/anudar/repository/AuctionWorkRepository.java
@@ -1,5 +1,7 @@
 package com.ssafy.anudar.repository;
 
+import com.ssafy.anudar.dto.AuctionDto;
+import com.ssafy.anudar.model.SuccessWork;
 import com.ssafy.anudar.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,9 +9,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface AuctionWorkRepository extends JpaRepository<AuctionWork, Long> {
-    List<AuctionWork> findByUser(User user);
+public interface AuctionWorkRepository extends JpaRepository<SuccessWork, Long> {
+    List<SuccessWork> findByUser(User user);
 
-    List<AuctionWork> findAllByUser(User user);
+    List<SuccessWork> findAllByUser(User user);
 
 }

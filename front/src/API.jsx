a228390b29cbf81befc8ad356504bcd0b5174cb0
sarang682 <jms20914@gmail.com>
2024-02-12 22:31:03 +0900

    feat: back: 도슨트 확인

--- front/src/API.jsx
+++ front/src/API.jsx
@@ -1,3 +1,66 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7a157f3cde5d54303d16caab3e3df8d3080750a4ecdc367c1d450b558bb866f4
-size 1879
+import axios from "axios";
+
+export const uploadimage = async (data) => {
+    const url = "/api/user/img";
+    const form = new FormData();
+    form.append("image", data);
+    return await axios.post(url, form)
+    .then(response => { return response.data? response.data : "" })
+    .catch((e) => { console.log(e); return ""; });
+};
+
+export const checkid = async (id) => {
+    const url = "/api/user/username";
+    return await axios.post(url, id)
+    .then(response => {
+        if (response.status === 200) return true;
+        else return false;
+    })
+    .catch((e) => { console.log(e); return false; });
+};
+
+export const checknickname = async (nickname) => {
+    const url = "/api/user/nickname";
+    return await axios.post(url, nickname)
+    .then(response => { 
+        if (response.status === 200) return true;
+        else return false;
+    })
+    .catch((e) => { console.log(e); return false; });
+};
+
+export const signup = async (userdata) => {
+    const url = "/api/user/join";
+    return await axios.post(url, userdata)
+    .then(response => { return response.data; })
+    .catch((e) => { console.log(e); });
+};
+
+export const login = async (id, password) => {
+    const url = "/api/user/login";
+    return await axios.post(url, {"username" : id, "password": password})
+    .then(response => { return response.data; })
+    .catch((e) => { console.log(e); return ""; });
+};
+
+export const myinfo = async (token) => {
+    if (token && token != "") {
+        const url = "/api/user/info";
+        return await axios.get(url, { headers: { Authorization: `Bearer ${token}`, } })
+        .then(response => { return response.data; })
+        .catch((e) => { console.log(e); return {}; });
+    }
+    else { alert("유효하지 않은 토큰입니다."); }
+};
+
+export const updateinfo = () => {
+
+};
+
+export const changepassword = () => {
+
+};
+
+export const signout = () => {
+
+};

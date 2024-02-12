a90a8c7537fadb60b2af7968f5309791896e5044
YUNA-AHN <yunanash1234@gmail.com>
2024-02-13 04:06:11 +0900

    merge: front : 닉네임 이슈 해결2

--- front/src/auctionlive/AuctionLivePage.jsx
+++ front/src/auctionlive/AuctionLivePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9586cc1ab30af9120f8a8c3adc63af1267a5deccff7d0c8b23bbca7c0fedeac4
-size 4756
+oid sha256:c7e1bb9b34ac1bc9f0063eb6898fcda5637e124dfd4635f8e6324f3f82ffc12e
+size 4757

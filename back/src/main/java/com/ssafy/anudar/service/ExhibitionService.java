dd4278380f4ccc641ea52b068ef3dfb208d51c40
sarang682 <jms20914@gmail.com>
2024-02-12 21:12:07 +0900

    fix: back: 도슨트 비디오 중복 저장 수정

--- back/src/main/java/com/ssafy/anudar/service/ExhibitionService.java
+++ back/src/main/java/com/ssafy/anudar/service/ExhibitionService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdff1ad9cc06cfb9268b848b23bd4962684046cff01272b9e798141339c19bd9
-size 5158
+oid sha256:900561c289cf1a3794323f94ceba4d93a6781a718689d7e1e68fc704c5dc734d
+size 5212

911d0503716b21ab29f1a4839c6ebf0e09a69064
suyeon-hyeon <shonee99@naver.com>
2024-02-12 19:20:20 +0900

    feat: 3D 전시작품 로컬 데이터

--- front/src/exhibit/Exhibit3DPage.jsx
+++ front/src/exhibit/Exhibit3DPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4cd76ad6ce47e7a064e52221d98ce3206fa3c89b17c80752f200fe8d34be511
-size 36569
+oid sha256:88d00071bb1928ee860ba99e7f9fd7ffab5400e9ed3b8c738347c0a639aa56ba
+size 36574

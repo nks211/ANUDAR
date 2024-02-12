017b12d8ead9089811a5d90727f849ed33efa495
suyeon-hyeon <shonee99@naver.com>
2024-02-12 18:42:16 +0900

    fix: 3D 전시 local 환경 완성

--- front/src/exhibit/Exhibit3DPage.jsx
+++ front/src/exhibit/Exhibit3DPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c18424643b2c44eb90f46d900ee4e16f6d102d14edd705ca40cb7e342d52336b
-size 30601
+oid sha256:2ab002d0f2867879d9a6ffa55b337864cc53272085a51bb6c562dcdbe1aae62d
+size 36570

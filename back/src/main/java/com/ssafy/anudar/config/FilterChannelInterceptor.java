4a42b3cf631fd904c7598e6da973fb23b515788f
안유나 <yunanash1234@gmail.com>
2024-02-13 16:01:29 +0900

    fix: back: 웹소켓 시큐리티

---
+++ back/src/main/java/com/ssafy/anudar/config/FilterChannelInterceptor.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2be1fa31ad2db4e1d1d2867767685f5d2453cd0493d5ae68b9da72931f0cc213
+size 2530
--- back/src/main/java/com/ssafy/anudar/config/WebSocketConfig.java
+++ back/src/main/java/com/ssafy/anudar/config/WebSocketConfig.java
@@ -1,26 +1,3 @@
-package com.ssafy.anudar.config;
-
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("*");
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
-    }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed14e2183b7ce43e3a613902f3fc31064848764751b4aabaf6a117282a370c54
+size 1460

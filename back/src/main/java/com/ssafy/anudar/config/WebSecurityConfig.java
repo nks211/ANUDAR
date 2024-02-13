89a84b4620a974ce40785a946dd2e0f946023d3d
sarang682 <jms20614@gmail.com>
2024-02-13 15:46:57 +0900

    fix: back: Security Config 수정

--- back/src/main/java/com/ssafy/anudar/config/WebSecurityConfig.java
+++ back/src/main/java/com/ssafy/anudar/config/WebSecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65b2c61b555d5262911f3d7997caaff276df2b12fcfbc3cf246446522afd4a2f
-size 3646
+oid sha256:2e4c7f24d347af33d8177acdbfb8c263878654c529499bc76e9b818d76ea561e
+size 3645

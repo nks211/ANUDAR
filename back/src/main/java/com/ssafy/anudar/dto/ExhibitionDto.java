df77ed4a37ded83be09894da4750c1cd99480724
sarang682 <jms20914@gmail.com>
2024-02-13 13:13:48 +0900

    fix: back: 도슨트 페이지 username 수정

--- back/src/main/java/com/ssafy/anudar/dto/ExhibitionDto.java
+++ back/src/main/java/com/ssafy/anudar/dto/ExhibitionDto.java
@@ -1,3 +1,36 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d633a592dfeaa32714b3ddaa2e9f3e99bf7c727c86032ad1b5d81f5bd165d4fa
-size 884
+package com.ssafy.anudar.dto;
+
+import com.ssafy.anudar.model.Exhibition;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExhibitionDto {
+    private Long id;
+    private String name;
+    private String detail;
+    private String image;
+    private LocalDateTime start_time;
+    private LocalDateTime end_time;
+    private String author;
+
+
+    public static ExhibitionDto fromEntity (Exhibition exhibition) {
+        return new ExhibitionDto(
+                exhibition.getId(),
+                exhibition.getName(),
+                exhibition.getDetail(),
+                exhibition.getImage(),
+                exhibition.getStart_time(),
+                exhibition.getEnd_time(),
+                exhibition.getUser().getName()
+        );
+    }
+
+}

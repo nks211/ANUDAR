28408583a53bd4d5bfd704fe1f131506740d6da8
안유나 <yunanash1234@gmail.com>
2024-02-13 17:34:06 +0900

    fix: front: 오류 수정 중

--- back/src/main/java/com/ssafy/anudar/dto/WorkDto.java
+++ back/src/main/java/com/ssafy/anudar/dto/WorkDto.java
@@ -1,32 +1,3 @@
-package com.ssafy.anudar.dto;
-
-import com.ssafy.anudar.model.Work;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class WorkDto {
-    private Long id;
-    private String title;
-    private String image;
-    private String detail;
-    private Integer price;
-    private int bid;
-    private String author;
-
-    public static WorkDto fromEntity (Work work) {
-        return new WorkDto(
-                work.getId(),
-                work.getTitle(),
-                work.getImage(),
-                work.getDetail(),
-                work.getPrice(),
-                work.getBid(),
-                work.getUser().getUsername()
-        );
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa41228a6ca110528afc3720ef2580ec40871bb89046f27a0ef63c6762080a5e
+size 873

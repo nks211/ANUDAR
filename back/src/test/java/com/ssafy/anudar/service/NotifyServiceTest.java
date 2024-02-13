bb3ab841302e26e35ff9fc5e93a278a77b1ea11a
sarang682 <jms20914@gmail.com>
2024-02-13 09:27:40 +0900

    fix: back: DB 변경

--- back/src/test/java/com/ssafy/anudar/service/NotifyServiceTest.java
+++ back/src/test/java/com/ssafy/anudar/service/NotifyServiceTest.java
@@ -1,3 +1,78 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0ccdf8d50446fd0a9401773166329152fb5042a21669e486e9e7e63337033420
-size 2734
+//package com.ssafy.anudar.service;
+//
+//import com.ssafy.anudar.model.Notify;
+//import com.ssafy.anudar.model.Notifytype;
+//import com.ssafy.anudar.model.User;
+//import com.ssafy.anudar.repository.NotifyRepository;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//public class NotifyServiceTest {
+//
+//    @Mock
+//    private NotifyRepository notifyRepository;
+//
+//    @InjectMocks
+//    private NotifyService notifyService;
+//
+//    @Test
+//    public void testCreateNotification() {
+//        User user = new User(); // 적절한 사용자 객체 생성
+//        String link = "https://example.com";
+//        Notifytype notifytype = Notifytype.AUTION_CREATED;
+//``
+//        notifyService.createdNotifications(user, notifytype, link);
+//
+//        // save 메서드가 1번 호출되었는지 검증
+//        verify(notifyRepository, times(1)).save(any(Notify.class));
+//    }
+//
+//    @Test
+//    public void testGetUnreadNotifications() {
+//        User user = new User(); // 적절한 사용자 객체 생성
+//        List<Notify> mockNotifications = new ArrayList<>();
+//        mockNotifications.add(new Notify());
+//        mockNotifications.add(new Notify());
+//
+//        // 사용자의 읽지 않은 알림을 목업 데이터로 설정
+//        when(notifyRepository.findByUserAndChecked(user, false)).thenReturn(mockNotifications);
+//
+//        List<Notify> result = notifyService.getUnreadNotifications(user);
+//
+//        // 반환된 알림 목록과 목업 데이터가 동일한지 검증
+//        assertThat(result).isEqualTo(mockNotifications);
+//    }
+//
+//    @Test
+//    public void testMarkNotificationAsRead() {
+//        Long notificationId = 1L;
+//        Notify mockNotify = new Notify();
+//
+//        // findById 메서드가 목업 데이터를 반환하도록 설정
+//        when(notifyRepository.findById(notificationId)).thenReturn(java.util.Optional.of(mockNotify));
+//
+//        notifyService.markNotificationAsRead(notificationId);
+//
+//        // read 메서드가 1번 호출되었는지 검증
+//        verify(mockNotify, times(1)).read();
+//    }
+//
+//    @Test
+//    public void testDeleteNotification() {
+//        Long notificationId = 1L;
+//
+//        notifyService.deleteNotification(notificationId);
+//
+//        // deleteById 메서드가 1번 호출되었는지 검증
+//        verify(notifyRepository, times(1)).deleteById(notificationId);
+//    }
+//}
--- front/src/StateManagement.jsx
+++ front/src/StateManagement.jsx
@@ -1,3 +1,203 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c6e89cf1f16d3a2eb6923c8cc4387388ffb0f5216fae7f9eeeafb355ea634560
-size 6881
+import { create } from "zustand";
+import { immer } from "zustand/middleware";
+
+// 로그인, 회원가입, 메뉴바, 로그인 모달 입력값 관련 상태 관리 함수
+export const mainstate = create((set) => ({
+    isLogin: localStorage.login === "true",
+    loginuser: localStorage.login === "true"?JSON.parse(localStorage.userdata):{},
+    logintoken: localStorage.login === "true"?localStorage.token:"",
+    signup: {
+        profileimage: "../../asset/profile_image.png",
+        id: "",
+        password: "",
+        passwordcheck: "",
+        name: "",
+        nickname: "",
+        birthday: "",
+        email: "",
+        number: "",
+    },
+    notice: 0,
+    noticelist: [
+        {
+            title: "알림 제목",
+            date: "2024/02/05 16:00",
+            details: "알림에 대한 자세한 설명 1",
+        },
+        {
+            title: "알림 제목",
+            date: "2024/02/05 16:00",
+            details: "알림에 대한 자세한 설명 2",
+        },
+        {
+            title: "알림 제목",
+            date: "2024/02/05 16:00",
+            details: "알림에 대한 자세한 설명 3",
+        },
+        {
+            title: "알림 제목",
+            date: "2024/02/05 16:00",
+            details: "알림에 대한 자세한 설명 4",
+        },
+        {
+            title: "알림 제목",
+            date: "2024/02/05 16:00",
+            details: "알림에 대한 자세한 설명 5",
+        },
+    ],
+    tabbar: ["전시회", "작가", "작품", "경매"],
+    menutab: "",
+    idinput: "",
+    passwordinput: "",
+
+    setIsLogin: (result) => set({ isLogin: result }),
+    // setIsLogin: (result) => set(() => ({ isLogin: result })),
+    setloginuser: (user) => set(() => ({ loginuser: user })),
+    setlogintoken: (token) => set(() => ({ logintoken: token })),
+    setprofileimage: (image) => set((state) => ({ signup: { ...state.signup, profileimage: image } })),
+    setidinput: (input) => set((state) => ({ signup: { ...state.signup, id: input } })),
+    setpasswordinput: (input) => set((state) => ({ signup: { ...state.signup, password: input } })),
+    setpasswordcheckinput: (input) => set((state) => ({ signup: { ...state.signup, passwordcheck: input } })),
+    setnameinput: (input) => set((state) => ({ signup: { ...state.signup, name: input } })),
+    setnicknameinput: (input) => set((state) => ({ signup: { ...state.signup, nickname: input } })),
+    setbirthdayinput: (input) => set((state) => ({ signup: { ...state.signup, birthday: input } })),
+    setemailinput: (input) => set((state) => ({ signup: { ...state.signup, email: input } })),
+    setnumberinput: (input) => set((state) => ({ signup: { ...state.signup, number: input } })),
+
+    setnotice: (result) => set(() => ({ notice: result })),
+    noticecheck: (notice) => set((state) => ({
+        noticelist: state.noticelist.filter((item) => { return JSON.stringify(item) != JSON.stringify(notice); })
+    })),
+    setmenutab: (tab) => set(() => ({ menutab: tab })),
+    setloginidinput: (input) => set(() => ({ idinput: input })),
+    setloginpasswordinput: (input) => set(() => ({ passwordinput: input })),
+    logincomplete: (loginid, loginpassword) => set((state) => ({ loginuser: { ...state.loginuser, id: loginid, password: loginpassword, } })),
+
+}));
+
+// 전시회 목록 및 상세 조회 관련 상태 관리 함수
+export const exhibitionstate = create((set) => ({
+
+}));
+
+// 작품 목록 및 상세 조회 관련 상태 관리 함수
+export const workstate = create((set) => ({
+
+}));
+
+// 작가 목록 및 상세 조회 관련 상태 관리 함수
+export const artiststate = create((set) => ({
+
+}));
+
+// 경매 관련 상태 관리 함수
+export const auctionstate = create((set) => ({
+
+}));
+
+// 마이페이지 관련 상태 관리 함수
+export const mypagestate = create((set) => ({
+
+    mytab: ["내 정보", "찜한 전시회", "찜한 작품", "팔로잉 목록", "경매 내역", "내 전시", "내 작품"],
+    mytabindex: "",
+    scroll: 0,
+    scrollref: {},
+    myeditmode: false,
+    updates: {
+        newnickname: "",
+        newphonenumber: "",
+        newpassword: "",
+        newpasswordcheck: "",
+        newprofileimage: "",
+        newemail: "",
+    },
+    myprofile: "",
+    myinfo: {
+        mynickname: "",
+        myfollower: 0,
+        myfollowing: 0,
+        myid: "",
+        mypassword: "",
+        mybirthday: "",
+        mynumber: "",
+    },
+    myfavorites: {
+        likeexhibitions: [],
+        likeworks: [],
+        followingartist: [],
+        myexhibitions: [],
+        myworks: [],
+        myauctions: [],
+    },
+    foldermode: [false, false, false, false, false, false,],
+
+    setmytabindex: (index) => set(() => ({ mytabindex: index })),
+    setscroll: (scrollnumber) => set(() => ({ scroll: scrollnumber })),
+    setmyeditmode: (result) => set(() => ({ myeditmode: result })),
+    setnewnickname: (input) => set((state) => ({ updates: { ...state.updates, newnickname: input } })),
+    setnewphonenumber: (input) => set((state) => ({ updates: { ...state.updates, newphonenumber: input } })),
+    setnewpassword: (input) => set((state) => ({ updates: { ...state.updates, newpassword: input } })),
+    setnewpasswordcheck: (input) => set((state) => ({ updates: { ...state.updates, newpasswordcheck: input } })),
+    passwordupdate: (oldpassword, newpassword) => {
+
+    },
+    setnewprofileimage: (input) => set((state) => ({ updates: { ...state.updates, newprofileimage: input } })),
+    setnewemail: (input) => set((state) => ({ updates: { ...state.updates, newemail: input } })),
+    getmyinfo: () => {
+        
+    },
+    updatemyinfo: (newinfo) => {
+
+    },
+    getmyfavorites: () => {
+
+    },
+    setfoldermode: (button) => {
+        
+    },
+    scrolltoref: (ref) => {
+
+    },
+
+}));
+
+// 팝업 모달창 관련 상태 관리 함수
+export const popupstate = create((set) => ({
+
+    homepopup: false,
+    homenoticepopup: false,
+    signuppopup: false,
+    mypagecheckpopup: false,
+    mypagechangepopup: false,
+    paymentPopup: false,
+
+    sethomepopup: (result) => set(() => ({ homepopup: result })),
+    sethomenoticepopup: (result) => set(() => ({ homenoticepopup: result })),
+    setsignuppopup: (result) => set(() => ({ signuppopup: result })),
+    setmypagecheckpopup: (result) => set(() => ({ mypagecheckpopup: result })),
+    setmypagechangepopup: (result) => set(() => ({ mypagechangepopup: result })),
+    setPaymentPopup: (result) => set(() => ({ paymentPopup: result })),
+
+}));
+
+// 도슨트 화면 관련 상태 관리 함수
+export const docentstate = create((set) => ({
+
+}));
+
+// 전시회 및 작품 등록 페이지 관련 상태 관리 함수
+export const registstate = create((set) => ({
+
+}));
+
+// 검색 입력창 관련 상태 관리 함수
+export const searchstate = create((set) => ({
+
+}));
+
+
+// export const today = 0;
+
+export function getToday() {
+    
+}

7e33fd7c5b9fed49321db5504d9250c0498b72fd
sarang682 <jms20914@gmail.com>
2024-02-14 13:48:50 +0900

    fix: front: 도슨트 페이지 import 수정

--- front/src/components/docent/DocentContents.js
+++ front/src/components/docent/DocentContents.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8e593fa16904b84c7b9e134e775f5577d7021774bcf678ad03cab344e034dcf
-size 3266
+oid sha256:e84a4740eb014485f9ba4cdb3d7063220cb9aef867208165658aaa1db6272275
+size 3274

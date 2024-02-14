201df5c4a3200b8da91dd001baa6959acc57e1d0
sarang682 <jms20914@gmail.com>
2024-02-14 17:26:30 +0900

    fix: front: 오픈비두 leaveSession 수정

--- front/src/components/docent/WebCam.js
+++ front/src/components/docent/WebCam.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18d85a66deeba81a159b91145a2bbf1ad987c355b9d2b771ba19232e0fadb434
-size 4841
+oid sha256:7d714c11bafa0fe22eb94987d29db7bf32fc961cf989ee478bb47b1003f5cc04
+size 5360

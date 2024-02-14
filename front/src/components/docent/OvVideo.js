63cfb30433a0d62c1efc2cec46efec1833d8938f
sarang682 <jms20914@gmail.com>
2024-02-14 21:29:10 +0900

    fix: front: 도슨트 화면 수정

--- front/src/components/docent/OvVideo.js
+++ front/src/components/docent/OvVideo.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f53c3c9cbb100f325c28bb2213d59d5edf26947978619cefe4ca53fd5ef0b720
+oid sha256:e37467ca4f3bf7af66b0aa8b068fdfa5e0e247c205a52c013bff866a228f739b
 size 745

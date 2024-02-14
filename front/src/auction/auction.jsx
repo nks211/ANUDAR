68fd2d2ccbddf069f4d2f3c1f82f4ef014e12750
김민우 <kmw7532@naver.com>
2024-02-14 12:41:41 +0900

    fix: homepage error 수정, 마이페이지 상단 메뉴탭 이슈 해결

--- front/src/auction/auction.jsx
+++ front/src/auction/auction.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd2abcf055a9516afacf9bb174bcdbbf00feed05dee1a24691541c954ca73f8b
-size 3986
+oid sha256:32889d029c1095f4199dc57f5155fa94c1303f7cfd5d944fabad6cdf98f25724
+size 3780

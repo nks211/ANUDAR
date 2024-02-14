140fadb334733cbc0ba7720fe7f233e47b595810
sarang682 <jms20914@gmail.com>
2024-02-14 14:27:13 +0900

    fix: front: 경매사 cam 크기 수정

--- front/src/components/auction/OvVideo.js
+++ front/src/components/auction/OvVideo.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a397ee3f14ceb4c0c07cc17b6575a5227656f73af3007a0fe7b3e490de78bd7a
+oid sha256:33d3b3b8f1130fa18e4f615adf49654f16c1fcd1f8bb77f9828c7fdb2334355d
 size 665

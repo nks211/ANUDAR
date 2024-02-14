5d8aa4d9cdad5ae8a44666d5ad2418b9d0ba48c8
suhyeon cho <jeniffer0812@naver.com>
2024-02-14 13:45:52 +0900

    fix: pay 확인중

--- front/src/components/payment/Payment.js
+++ front/src/components/payment/Payment.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42ff3726d89592214b8d7dd0c5e56b333d2e781ed6cef521705937be7f569ba3
-size 3449
+oid sha256:8fadc58717d9208a165bca15abea2d1f5f7e357027b6557cf467f32902f68ee8
+size 3365

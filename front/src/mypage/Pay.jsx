4f422501ebe16c76671cc037956d871b6db758f3
suhyeon cho <jeniffer0812@naver.com>
2024-02-14 13:29:19 +0900

    fix: pay 확인중

--- front/src/mypage/Pay.jsx
+++ front/src/mypage/Pay.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c44e245000fe479b6ad4bbbf08a3680ea5a2d45527aaf13fd88f16546ea28f0
-size 1823
+oid sha256:59ebd58d5ffb3fdce025b96fa8b7450e2ef448a5208e6a5b7c80e93ab768b0d9
+size 1802

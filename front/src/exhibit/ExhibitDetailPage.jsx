8b07adfb9e373e97bd0594c8fc0a26bb4c830d71
박규리 <dw05131@naver.com>
2024-02-14 14:17:03 +0900

    fix: 필요없는 코드 삭제

--- front/src/exhibit/ExhibitDetailPage.jsx
+++ front/src/exhibit/ExhibitDetailPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e40f05ad8304ba6f25efde44f257b6c8bdec05fe17e634b3b184b49fe8cc8191
-size 4589
+oid sha256:11300017ce86174225f5b8d04e484d5c812a00cf569d321f786cf9f5d279b20e
+size 4312
--- front/src/work/WorkPage.jsx
+++ front/src/work/WorkPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bc7a73fe058f8bbde66f7f863dc1eaf1e70b91e9fca3d5511a6282e662c8bde
-size 2185
+oid sha256:3f2fad9ea3357a6ce2b6f1ec1d9db8bd53eb61a0ecd007d515ae6179c18ee388
+size 1693

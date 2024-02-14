a2c902616dabb5934d8fc262b35833c1153c81cf
suyeon-hyeon <shonee99@naver.com>
2024-02-14 12:43:03 +0900

    fix: 전시회 등록 및 전시 상세 조회 - work entity에 캐러셀 칼럼 추가 - 등록할 때 캐러셀 여부 작성 추가 - 상세 조회 시, 캐러셀에 들어갈 작품 목록 조회 추가

--- back/src/main/java/com/ssafy/anudar/service/ExhibitionService.java
+++ back/src/main/java/com/ssafy/anudar/service/ExhibitionService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bd21e117985bfc9cc2ea3d024fb7ae6f0647d37a47ce7ab0b498c0b0ff9f4ff
-size 5494
+oid sha256:b56fbc799139693b546b381f30ba15dad197f1be8a8a584faf4851728deb9726
+size 5601

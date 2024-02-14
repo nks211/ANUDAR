474354f333c363ae66bdf7c065f13e703e9c95ca
sarang682 <jms20914@gmail.com>
2024-02-14 11:13:47 +0900

    fix: front: 도슨트 페이지 에러 수정

--- front/src/components/docent/DocentContents.js
+++ front/src/components/docent/DocentContents.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a2236bea1181ab4897e3539af0d726c9cd5ae6a44fd0ec90fe51ee9963af521
-size 2823
+oid sha256:4fc05fabd7fe5456c8a8dae719d73a8152cbc7a7e3da23f52ec3d4ebd872122d
+size 2876

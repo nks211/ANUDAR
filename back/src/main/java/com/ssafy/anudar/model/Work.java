7dc6cbf0ebedad5572b02bd0cbaf47543607a149
suyeon-hyeon <shonee99@naver.com>
2024-02-14 14:03:52 +0900

    fix: User와 Auction Entity 정리 및 yml 배포 버전으로 수정

--- back/src/main/java/com/ssafy/anudar/model/Work.java
+++ back/src/main/java/com/ssafy/anudar/model/Work.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51fc30e41ce1e62e54ccae5404a30a0d8c504230b1550770b76c3506bed9d3d2
-size 1559
+oid sha256:f6bced3ff391ed621d34c73cd0de7f1618770c3fdcabcae4e5d8c8527398f677
+size 1453

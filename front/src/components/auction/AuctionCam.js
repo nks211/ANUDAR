b94a5ede2c78c80507a6d72d219cf5ab8da02315
sarang682 <jms20614@gmail.com>
2024-02-14 09:37:59 +0900

    fix: front: 경매페이지 오픈비두 수정

--- front/src/components/auction/AuctionCam.js
+++ front/src/components/auction/AuctionCam.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b260e2513cca06f9d27fc183a8a578e64d6d08f0da0dbead0e42ea26a55db44
-size 3837
+oid sha256:c97ecb9770d6902691ff4ed1afd421fa59f22a61d7d6a250c2eefcb6917eabf8
+size 3883

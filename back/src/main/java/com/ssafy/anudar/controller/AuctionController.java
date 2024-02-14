b7bc3c5a96819214ed78aaf646d0a863cb3905b4
suhyeon cho <jeniffer0812@naver.com>
2024-02-14 11:00:05 +0900

    pay 마무리

--- back/src/main/java/com/ssafy/anudar/controller/AuctionController.java
+++ back/src/main/java/com/ssafy/anudar/controller/AuctionController.java
@@ -1,3 +1,37 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:de696d53fb4418e65717fdbc0bb72732866a54c4347b8e711e31e545c72a458e
-size 1188
+package com.ssafy.anudar.controller;
+
+import com.ssafy.anudar.dto.SuccessWorkDto;
+import com.ssafy.anudar.dto.WorkDto;
+import com.ssafy.anudar.dto.request.SuccessWorkRequset;
+
+import com.ssafy.anudar.service.AuctionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@CrossOrigin(allowedHeaders = "*", originPatterns = "*")
+@RestController
+@RequestMapping("/auction")
+@RequiredArgsConstructor
+public class AuctionController {
+
+    private final AuctionService auctionService;
+    @PostMapping("/bidok")
+    public ResponseEntity<SuccessWorkDto> bidok(@RequestBody SuccessWorkRequset req) {
+        SuccessWorkDto successWorkDto = auctionService
+                .saveSuccessWork(req.getWorkId(), req.getNickname(),
+                        req.getAuctionId(), req.getFinalPrice());
+        return new ResponseEntity<>(successWorkDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/works")
+    public ResponseEntity<List<WorkDto>> getWorks() {
+        return new ResponseEntity<>(auctionService.getWorks(), HttpStatus.OK);
+    }
+
+}

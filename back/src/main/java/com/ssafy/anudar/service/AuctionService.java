b7cd9318430139502151154371722b88b6f4aaf7
안유나 <yunanash1234@gmail.com>
2024-02-15 14:50:43 +0900

    feat: front: 포인트 제한 추가

--- back/src/main/java/com/ssafy/anudar/service/AuctionService.java
+++ back/src/main/java/com/ssafy/anudar/service/AuctionService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:446ab48ac9989c0671b4227aefbfe2a34f4f9663064303f7ca3231eec9a17fd7
-size 3150
+oid sha256:80dba22e3ffc1867a6b0088c46d34a9a76cb86a2c31288669c54bca3d29cc8eb
+size 3142
--- front/src/API.jsx
+++ front/src/API.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:567d2817b30c6174ed99ab4ecb17f3835efe81d3cf77eaef35571ecb2e9cb482
-size 13321
+oid sha256:3b9396e9aa1de3ef8a2f8b5aa52a5713a7847a61b2c96be277331fdb6bff5e05
+size 13776

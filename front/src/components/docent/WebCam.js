477c09531a0baf19b3f0db0da1f7929f6f86186e
sarang682 <jms20914@gmail.com>
2024-02-15 20:25:13 +0900

    fix: front: import 수정

--- front/src/components/docent/WebCam.js
+++ front/src/components/docent/WebCam.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { OpenVidu } from 'openvidu-browser';
 import axios from 'axios';
 import UserVideoComponent from './UserVideoComponent';

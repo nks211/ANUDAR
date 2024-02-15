ab334fbc31750c0b7cfecbde093259f711f79213
sarang682 <jms20914@gmail.com>
2024-02-15 20:35:50 +0900

    fix: front: cam on/off 수정

--- front/src/components/docent/WebCam.js
+++ front/src/components/docent/WebCam.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a54fa2f5559932f20c9fefa984810afd45faae12ad5d81a89ed55c86585acac
-size 5202
+oid sha256:09b55f07b2fccbfe4f7dab34a0c6722aa313a2ce733477d12dd682332ddf6096
+size 5204

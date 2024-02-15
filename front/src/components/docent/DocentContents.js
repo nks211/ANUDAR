ba7cc985dab43eca95fb9fd28546dd1a347ddadc
sarang682 <jms20914@gmail.com>
2024-02-15 09:47:20 +0900

    fix: front: 도슨트 채팅 디자인 수정

--- front/src/components/docent/DocentContents.js
+++ front/src/components/docent/DocentContents.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5711f91b813f81848c063155897df966f2eeca898077e326558b8ed5f97c9a70
-size 3310
+oid sha256:5b90ba1edcd7cbd89449f935b235a3f6b4f3cc31ff2205dc64338ad5090f0de2
+size 3004

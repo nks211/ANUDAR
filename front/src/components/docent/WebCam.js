89fae0744ba7f2e7cd3fade808ffdde7437321b5
sarang682 <jms20914@gmail.com>
2024-02-15 20:25:13 +0900

    fix: front: import 수정

--- front/src/components/docent/WebCam.js
+++ front/src/components/docent/WebCam.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8184751f5f447940802166468313e37b09858d4a9a2c0ea1f23e5a33238a91a
-size 5190
+oid sha256:3a54fa2f5559932f20c9fefa984810afd45faae12ad5d81a89ed55c86585acac
+size 5202

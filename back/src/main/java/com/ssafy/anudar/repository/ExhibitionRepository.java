699f7be3fb0683d01e45447d977cbf94f23f9cf8
박규리 <dw05131@naver.com>
2024-02-15 10:24:34 +0900

    fix: 전시회 등록 API, 찜팔로우, 캐러셀 컴포넌트 수정

--- back/src/main/java/com/ssafy/anudar/repository/ExhibitionRepository.java
+++ back/src/main/java/com/ssafy/anudar/repository/ExhibitionRepository.java
@@ -1,12 +1,14 @@
 package com.ssafy.anudar.repository;
 
 import com.ssafy.anudar.model.Exhibition;
+import com.ssafy.anudar.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -15,4 +17,5 @@ public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
     @Query("SELECT e FROM Exhibition e WHERE e.start_time <= :now AND e.end_time >= :now")
     List<Exhibition> findExhibitionsByCurrentTime(LocalDateTime now);
 
+    List<Exhibition> findAllByUser(User user);
 }

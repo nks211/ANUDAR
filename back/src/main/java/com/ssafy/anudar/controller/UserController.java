019a1ccea36b39f40f1d569d0e2e09e9775b68b9
suhyeon cho <jeniffer0812@naver.com>
2024-02-16 01:55:51 +0900

    fix: mypage 수정

--- back/src/main/java/com/ssafy/anudar/controller/UserController.java
+++ back/src/main/java/com/ssafy/anudar/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed0105ed76f89db5bf33ccfa337fd796864a0e78d0a1fcf1b9de911095ff5d35
-size 8058
+oid sha256:d6872718b579a7fb40baccf226398beb161d482256609ade6670f6337929f799
+size 8072
--- back/src/main/java/com/ssafy/anudar/repository/DocentRepository.java
+++ back/src/main/java/com/ssafy/anudar/repository/DocentRepository.java
@@ -1,8 +1,3 @@
-package com.ssafy.anudar.repository;
-
-import com.ssafy.anudar.model.Docent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocentRepository extends JpaRepository <Docent, Long> {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4464c504151aea96208391a9a81b8e7083300d7d259f7aaa09de2ec2b3ade065
+size 215

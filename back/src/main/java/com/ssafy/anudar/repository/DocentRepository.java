cd94df2d5d12349690d2f478c8dbfc4220119260
sarang682 <jms20914@gmail.com>
2024-02-16 04:20:14 +0900

    fix: back: 도슨트 API 수정

--- back/src/main/java/com/ssafy/anudar/repository/DocentRepository.java
+++ back/src/main/java/com/ssafy/anudar/repository/DocentRepository.java
@@ -1,8 +1,3 @@
-package com.ssafy.anudar.repository;
-
-import com.ssafy.anudar.model.Docent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocentRepository extends JpaRepository <Docent, Long> {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:051cfa737bcad8f6c0cd547df4672e2af5416195c25fc84077809dd9d01acc10
+size 303

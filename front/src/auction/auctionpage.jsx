216eeac416233a4b263c7abdaadb1fbe5a3f4ecb
안유나 <yunanash1234@gmail.com>
2024-02-15 10:55:39 +0900

    fix: front: 낙찰 페이지 수정

--- front/src/auction/auctionpage.jsx
+++ front/src/auction/auctionpage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e46a38fd6d8d937ac624d9fbc240769dbb65939001e187f2ca0aae163d1a968c
-size 11654
+oid sha256:6e699cc79de91ab2349598faf46dc0adaef7d38cbb25dd3fb0fee7028c1588f3
+size 10927

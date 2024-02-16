aafe33ccb31918d28084959630aac31cd0c51d4c
안유나 <yunanash1234@gmail.com>
2024-02-16 11:44:31 +0900

    testing plz

--- front/src/auction/auction.jsx
+++ front/src/auction/auction.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb08614fd4abf1f98d92be3691b4cc923d4251269653d83d7f0b5584191350eb
-size 4782
+oid sha256:f4224b170b021f98c4170a1fcab75b955d772a06a4fbc1930048cd548fe40cd4
+size 4845

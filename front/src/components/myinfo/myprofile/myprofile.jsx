0dee79387f3409c77c8f76deacb687a01f2aa3f4
suhyeon cho <jeniffer0812@naver.com>
2024-02-19 09:23:52 +0900

    fix: pay

--- front/src/components/myinfo/myprofile/myprofile.jsx
+++ front/src/components/myinfo/myprofile/myprofile.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0815c7dee082cde9a67d6ad3826eed95c668c3a58564f3c71446c8d9aa4e7a9
-size 3304
+oid sha256:6cdf16e228915f7d99af1b5401c6b4aeb64b1cfe95f3d7abfaffdac5595fb8d5
+size 3199

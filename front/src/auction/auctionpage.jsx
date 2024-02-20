754b11c6e6352ad08f45559b910808059ead637c
박규리 <dw05131@naver.com>
2024-02-20 10:00:26 +0900

    merge

--- front/src/auction/auctionpage.jsx
+++ front/src/auction/auctionpage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d04bb06dd7cd6a4a8e538d3688142c93652c7b4592ab18e47eaf121290ac2ad
-size 11034
+oid sha256:5221cb3e5226087ca7d9c87c4befbcc0b052990883eb64911be0af5ad85f95a3
+size 11009
